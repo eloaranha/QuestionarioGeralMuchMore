4b9764e5a3348d0753017d0152e9261d2e7dd700
Eloísa Cruz <eloisanerizaranhadacruz@gmail.com>
2020-10-07 07:50:18 -0300

    Questionario

---
+++ QuestionarioGeralMuchMore/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuestionarioGeralMuchMore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Questionário de conhecimentos Gerais";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Entre em Contato";
+
+            return View();
+        }
+    }
+}
